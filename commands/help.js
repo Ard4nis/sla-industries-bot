38ce9431e589ac9bcbb71a1b03f3da51ff2bce64
Ardanis <kesw34@gmail.com>
2020-04-14 01:01:44 +0200

    Updated help to send a DM in all success cases.

--- commands/help.js
+++ commands/help.js
@@ -14,17 +14,8 @@ module.exports = {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-			return message.author.send(data, { split: true })
-				.then(() => {
-					if (message.channel.type === 'dm') return;
-					message.reply('I\'ve sent you a DM with all my commands!');
-				})
-				.catch(error => {
-					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-				});
 		}
+		else {
 		const name = args[0].toLowerCase();
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
@@ -39,7 +30,16 @@ module.exports = {
 		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		}
 
-		message.channel.send(data, { split: true });
+		message.author.send(data, { split: true })
+			.then(() => {
+				if (message.channel.type === 'dm') return;
+				message.reply('I\'ve sent you a DM with your help information.');
+			})
+			.catch(error => {
+				console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+				message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+			});
 	},
 };
