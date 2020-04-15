910e5d94e9210747bc94ddaed1f3a1dfc4fca0e9
Kes Lagoni Williams <kes.l.williams@jp.dk>
2020-04-15 12:56:17 +0200

    Cooldown wait message is now sent as whisper instead of reply in channel to limit spam.

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on('message', message => {
 
 		if (now < expirationTime) {
 			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+			return message.author.send(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
 		}
 	}
 	timestamps.set(message.author.id, now);
